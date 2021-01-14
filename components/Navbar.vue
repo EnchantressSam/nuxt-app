33293b58b2929ea4b8c90ad6c337ef1565314515
Tatiana Loginova <tatyanka8samara@mail.ru>
2021-01-14 15:39:56 +0400

    created navbar, error page

---
+++ components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Nuxt SSR</a>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <nuxt-link
+            exact
+            no-prefetch
+            active-class="active"
+            class="nav-link"
+            to="/"
+            >Home</nuxt-link
+          >
+        </li>
+        <li class="nav-item">
+          <nuxt-link active-class="active" class="nav-link" to="/about"
+            >About</nuxt-link
+          >
+        </li>
+        <li class="nav-item">
+          <nuxt-link active-class="active" class="nav-link" to="/users"
+            >Users</nuxt-link
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1>About</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
+      repudiandae ratione esse beatae totam eius laboriosam dolor consequatur
+      minima voluptas.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis illum
+      sapiente nam eaque ut?
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, odio.</p>
+  </section>
+</template>
