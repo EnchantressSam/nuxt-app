03a1b03aeefd3834f3a304a644273bd4d6d513c8
Tatiana Loginova <tatyanka8samara@mail.ru>
2021-01-19 01:40:58 +0400

    created store

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,6 +1,9 @@
 <template>
   <section>
-    <h1>User {{ $route.params.id }}</h1>
+    <h1>{{ user.name }}</h1>
+    <hr />
+
+    <h3>{{ user.email }}</h3>
   </section>
 </template>
 
@@ -8,6 +11,12 @@
 export default {
   validate({ params }) {
     return /^\d+$/.test(params.id);
+  },
+  async asyncData({ $axios, params }) {
+    const user = await $axios.$get(
+      "https://jsonplaceholder.typicode.com/users/" + params.id
+    );
+    return { user };
   },
 };
 </script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -11,21 +11,25 @@
 
 <script>
 export default {
-  async asyncData({ $axios }) {
-    const users = await $axios.$get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    return { users };
+  async fetch({ store }) {
+    if (store.getters["users/users"].length === 0) {
+      await store.dispatch("users/fetch");
+    }
   },
   data() {
     return {
       pageTitle: "Users",
     };
   },
+  computed: {
+    users() {
+      return this.$store.getters["users/users"];
+    },
+  },
 
   methods: {
     openUser(user) {
-      this.$router.push("/users/" + user);
+      this.$router.push("/users/" + user.id);
     },
   },
 };
